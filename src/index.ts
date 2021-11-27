ff0baa94495d1c5d5932ef32f20f9d6fc762d89c
Souvikns <souvikde.ns@gmail.com>
2021-11-27 13:17:26 +0530

    chore: cleaning up the codebase

--- src/index.ts
+++ src/index.ts
@@ -1,59 +1,30 @@
 import * as core from '@actions/core';
 import * as github from '@actions/github';
-import { eventType, getIssue } from './util';
-import { Issue, Issues } from './models';
-import { Notion } from './notion';
-import {NotionAdapter} from './adapter';
-import {App} from './app';
+import App from './app';
+import { NotionAdapter } from './notion';
+import { getIssue } from './model';
+
 
 const token = core.getInput('token') || process.env.GH_PAT || process.env.GITHUB_TOKEN;
-const eventName = process.env.GITHUB_EVENT_NAME;
 const notionApiKey = process.env.NOTION_API_KEY || core.getInput('NOTION_API_KEY');
 const notionDatabase = process.env.NOTION_DATABASE || core.getInput('NOTION_DATABASE');
 
-export const run = async () => {
-	if (!token) throw new Error("⛔ Github token not found");
-	if (!notionApiKey) throw new Error("⛔ Notion API Key missing");
-	if (!notionDatabase) throw new Error("⛔ Notion Database ID missing");
-	const app = new App(new NotionAdapter(notionApiKey, notionDatabase));
-	const action = github.context.payload.action;
-	if (eventName === 'workflow_dispatch') {
-		const {response, error} = await app.initialize();
-		if(error) throw error;
-		console.log(response);
-		return;
-	}
-	if (!eventName || !action) throw new Error("⛔ Event Name or action missing");
-	await app.IssueActionHandler(eventType(eventName, action), getIssue(github.context.payload.issue));
+async function run() {
+    if (!token) throw new Error("Github TOken not found");
+    if (!notionApiKey) throw new Error("Notion api key not found");
+    if (!notionDatabase) throw new Error("Notion database id missing");
+    const action = github.context.payload.action;
+    const eventName = github.context.eventName;
+
+    const app = new App(new NotionAdapter(notionApiKey, notionDatabase));
+    if (!eventName || !action) throw new Error('Event name or action missing');
+    app.run(`${eventName}.${action}`, getIssue(github.context.payload.issue));
 }
 
-const main = async (eventType: string, issue: Issue) => {
-	let notion = await Notion(notionApiKey, notionDatabase, issue);
-	if (eventType.split('.')[0] === 'issues') {
-		switch (eventType) {
-			case Issues().opened():
-				return await notion.issueCreated();
-			case Issues().closed():
-				return await notion.issueClosed();
-			case Issues().edited():
-				return await notion.issueEdited();
-			case Issues().deleted():
-				return await notion.issueDeleted();
-			case Issues().reopened():
-				return await notion.issueReopened();
-			case Issues().labeled():
-				return await notion.issueLabeled();
-			case Issues().unlabeled():
-				return await notion.issueUnlabeled();
-			default:
-				console.log("Something happend that I am not accountable for");
-		}
-	}
-}
 
 run()
-	.then(() => { })
-	.catch((err) => {
-		console.log("ERROR", err);
-		core.setFailed(err.message);
-	})
+    .then()
+    .catch(e => {
+        console.error("ERROR: ", e);
+        core.setFailed(e.message);
+    });
--- src/notion.ts
+++ src/notion.ts
@@ -1,196 +1,111 @@
 import { Client } from '@notionhq/client';
-import { Issue } from './models';
+import { IPageInput, IssueState } from './model';
 
-const notionApi = async (apiKey: string, database_id: string) => {
-	let notion = await new Client({ auth: apiKey });
-	return {
-		createPage: async (name: string, url: string, id: number, state: string, body: string) => {
-			try {
-				let response = await notion.pages.create({
-					parent: {
-						database_id: database_id
-					},
-					properties: {
-						//@ts-ignore
-						Name: {
-							title: [
-								{ text: { content: name }, type: 'text' }
-							]
-						},
-						//@ts-ignore
-						'URL': {
-							url: url
-						},
-						// @ts-ignore
-						id: {
-							number: id
-						},
-						//@ts-ignore
-						state: {
-							//@ts-ignore
-							select: {
-								name: state
-							}
-						},
-						body: {
-							rich_text: [
-								{ text: { content: (body != undefined) ? body : '' }, type: 'text' }
-							],
-							type: 'rich_text'
-						}
-					}
-				})
-
-				if (response) return "✔ page created";
-			} catch (error) {
-				throw error;
-			}
-		},
-		updateLabel: async (labels: any, id: number) => {
-			console.log(labels, id);
-			try {
-				let LabelList = labels.map((el: any) => ({ name: el.name }));
-				const response = await notion.databases.query({
-					database_id: database_id,
-					filter: {
-						property: 'id',
-						number: {
-							equals: id
-						}
-					}
-				});
-				let pageID = response.results[0].id;
-				let res = await notion.pages.update({
-					page_id: pageID,
-					properties: {
-						//@ts-ignore
-						labels: {
-							multi_select: LabelList
-						}
-					}
-				});
-				if (res) return "✔ labels updated"
-			} catch (error) {
-				throw error;
-			}
-
-		},
-		updatePage: async (name: string, id: number, body: string) => {
-			const response = await notion.databases.query({
-				database_id: database_id,
-				filter: {
-					property: 'id',
-					number: {
-						equals: id
-					}
-				}
-			});
-			let pageID = response.results[0].id;
-
-			let res = await notion.pages.update({
-				page_id: pageID,
-				properties: {
-					//@ts-ignore
-					Name: {
-						title: [
-							{ text: { content: name }, type: 'text' }
-						]
-					},
-					body: {
-						rich_text: [
-							{ text: { content: (body != undefined) ? body : '' }, type: 'text' }
-						],
-						type: 'rich_text'
-					}
-				}
-			});
-			if (res) return "✔ page updated"
-		},
-		updateState: async (state: string, id: number) => {
-			try {
-				const response = await notion.databases.query({
-					database_id: database_id,
-					filter: {
-						property: 'id',
-						number: {
-							equals: id
-						}
-					}
-				});
-				let pageID = response.results[0].id;
-
-				let res = await notion.pages.update({
-					page_id: pageID,
-					properties: {
-						//@ts-ignore
-						state: {
-							//@ts-ignore
-							select: {
-								name: state
-							}
-						}
-					}
-				});
-				if (res) return "✔ state updated"
-			} catch (error) {
-				throw error
-			}
-		}
-	}
+class NotionCLient {
+    protected client: Client;
+    constructor(apiKey: string) {
+        this.client = new Client({ auth: apiKey });
+    }
 }
 
-export const Notion = async (api_key: string, database_id: string, issue: Issue) => {
-	const notion = await notionApi(api_key, database_id);
-	return {
-		issueCreated: async () => {
-			//TODO: Create a page in notion 
-			try {
-				let res = await notion.createPage(issue.title, issue.html_url, issue.id, issue.state, issue.body);
-				console.log(res);
-				res = await notion.updateLabel(issue.labels, issue.id);
-				console.log(res);
-			} catch (error) {
-				throw error
-			}
-		},
-		issueEdited: async () => {
-			try {
-				let res = notion.updatePage(issue.title, issue.id, issue.body);
-				console.log(res);
-			} catch (error) {
-				throw error;
-			}
-		},
-		issueClosed: async () => {
-			try {
-				let res = await notion.updateState(issue.state, issue.id);
-				console.log(res);
-			} catch (error) {
-				throw error
-			}
-		},
-		issueDeleted: async () => {
+export class NotionAdapter extends NotionCLient {
+    constructor(apiKey: string, private databaseId: string) {
+        super(apiKey);
+    }
 
-		},
-		issueReopened: async () => {
-			let res = await notion.updateState(issue.state, issue.id);
-			console.log(res);
-		},
-		issueLabeled: async () => {
-			try {
-				let res = await notion.updateLabel(issue.labels, issue.id);
-				console.log(res);
-			} catch (error) {
-				throw error;
-			}
-		},
-		issueUnlabeled: async () => {
-			try {
-				let res = await notion.updateLabel(issue.labels, issue.id);
-				console.log(res);
-			} catch (error) {
-				throw error;
-			}
-		}
-	}
+    async createPage(input: IPageInput) {
+        await this.client.pages.create({
+            parent: {
+                database_id: this.databaseId
+            },
+            properties: {
+                Name: {
+                    title: [
+                        { text: { content: input.title }, type: 'text' }
+                    ]
+                },
+                URL: {
+                    url: input.url
+                },
+                ID: {
+                    number: input.id
+                },
+                State: {
+                    select: { name: input.state }
+                }
+            }
+        })
+    }
+
+    async updatePage(id: number, title: string) {
+        const pages = await this.client.databases.query({
+            database_id: this.databaseId,
+            filter: {
+                property: 'ID',
+                number: {
+                    equals: id
+                }
+            }
+        });
+
+        const pageId = pages.results[0].id;
+
+        await this.client.pages.update({
+            page_id: pageId,
+            properties: {
+                Name: {
+                    title: [
+                        { text: { content: title }, type: 'text' }
+                    ]
+                }
+            }
+        });
+    }
+
+    async updateState(id: number, state: IssueState) {
+        const pages = await this.client.databases.query({
+            database_id: this.databaseId,
+            filter: {
+                property: 'ID',
+                number: {
+                    equals: id
+                }
+            }
+        });
+        const pageId = pages.results[0].id;
+
+        await this.client.pages.update({
+            page_id: pageId,
+            properties: {
+                state: {
+                    select: {
+                        name: state
+                    }
+                }
+            }
+        });
+    }
+
+    async updateLabel(id: number, labels: any) {
+        const labelList = labels.map((el: any) => ({ name: el.name }));
+        const pages = await this.client.databases.query({
+            database_id: this.databaseId,
+            filter: {
+                property: 'ID',
+                number: {
+                    equals: id
+                }
+            }
+        });
+        const pageId = pages.results[0].id;
+
+        await this.client.pages.update({
+            page_id: pageId,
+            properties: {
+                Label: {
+                    multi_select: labelList
+                }
+            }
+        });
+    }
 }
